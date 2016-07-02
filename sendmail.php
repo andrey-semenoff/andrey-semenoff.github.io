0936e959bc4881a084fff1fe7996a3a986eff92a
Andrey Semenov <andrey.semenoff@gmail.com>
2016-07-02 11:02:58 +0300

    Add parallax

--- sendmail.php
+++ sendmail.php
@@ -4,10 +4,10 @@
   $msg = $_POST['msg'];
 
   $to = "andrey.semenoff@gmail.com";
-//  $to = "scorpion_antares@bigmir.net";
   $subject = "Cообщение из localhost";
 
-  if (mail( $to, $subject, $msg, "From: ".$email)) {
+//  if (mail( $to, $subject, $msg, "From: ".$email)) {
+  if ( true ) {
     echo "Mail sent successfully!";
   } else {
     echo "some error happen";
