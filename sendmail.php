50d859acf85376ac19fa858bb16103e887514990
Andrey Semenov <andrey.semenoff@gmail.com>
2016-07-11 22:33:14 +0300

    fix some bugs on blog page

--- sendmail.php
+++ sendmail.php
@@ -4,10 +4,10 @@
   $msg = $_POST['msg'];
 
   $to = "andrey.semenoff@gmail.com";
+//  $to = "scorpion_antares@bigmir.net";
   $subject = "Cообщение из localhost";
 
-//  if (mail( $to, $subject, $msg, "From: ".$email)) {
-  if ( true ) {
+  if (mail( $to, $subject, $msg, "From: ".$email)) {
     echo "Mail sent successfully!";
   } else {
     echo "some error happen";
